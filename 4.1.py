38e0bb23a9789b8ef525aea03b36ed550bffc0b2
Шулекин Даниил <shulekin.2016@mail.ru>
2026-08-16 16:09:32 +0700

    Урок 4 задание 1

---
+++ 4.1.py
@@ -0,0 +1,6 @@
+a = float(input("Введите длину стороны A: "))
+b = float(input("Введите длину стороны B: "))
+area = a * b
+perimeter = 2 * (a + b)
+print("Площадь прямоугольника:", area)
+print("Периметр прямоугольника:", perimeter)
